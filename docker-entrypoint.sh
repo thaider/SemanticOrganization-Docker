64c54f46345bc7953ad6e61058ce63750c666dfb
Tobias Haider <thai@kollabor.at>
2021-05-07 23:30:31 +0200

    reorganizing entrypoint, implementing debug mode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,25 +11,32 @@ if [ ! -e $CONTAINER_INSTALLED ]; then
     echo "SETUP (SEMANTIC-)MEDIAWIKI..."
     php maintenance/install.php --dbserver=$MYSQL_HOST --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --scriptpath="" --lang=$MEDIAWIKI_LANG --pass=$MEDIAWIKI_ADMIN_PASSWORD "$MEDIAWIKI_NAME" "$MEDIAWIKI_ADMIN_USERNAME"
 
+    echo "SAVE LOCALSETTINGS.PHP"
     cp -a LocalSettings.php config/
 
     touch $CONTAINER_INSTALLED
 
 fi
 
+echo "RESET/UPDATE LOCALSETTINGS.PHP"
+cp -a config/LocalSettings.php ./ 
+echo "\$wgServer = \"$MEDIAWIKI_SERVER\";" >> LocalSettings.php
+echo "require_once('LocalSettings.additional.php');" >> LocalSettings.php
+echo "require_once('config/LocalSettings.override.php');" >> LocalSettings.php  
+cp templates/config/LocalSettings.additional.template.php LocalSettings.additional.php 
+
+if [ $MEDIAWIKI_DEBUG == 'true' ]; then
+    
+    echo "ENABLE DEBUG MODE..."
+    echo "\$wgShowExceptionDetails = true;" >> LocalSettings.php 
+    echo "\$wgShowDBErrorBacktrace = true;" >> LocalSettings.php 
+
+fi
+
 if [ ! -e $CONTAINER_UPDATED ]; then
 
-    cp -a config/LocalSettings.php ./
-
-    php maintenance/update.php --skip-external-dependencies
-
-    echo " " >> LocalSettings.php
-    echo "require_once('LocalSettings.additional.php');" >> LocalSettings.php    
-
-    echo "UPDATE LOCALSETTINGS.PHP..."
-    cp templates/config/LocalSettings.additional.template.php LocalSettings.additional.php
-
-    echo "require_once('config/LocalSettings.override.php');" >> LocalSettings.php    
+    echo "RUN MEDIAWIKI UPDATE SCRIPT..."
+    php maintenance/update.php --skip-external-dependencies --quick
 
     echo "IMPORTING SEMORG PAGES..."
     php maintenance/importDump.php < extensions/SemanticOrganization/import/semorg_pages.xml
@@ -39,6 +46,7 @@ if [ ! -e $CONTAINER_UPDATED ]; then
     php maintenance/runJobs.php
 
     touch $CONTAINER_UPDATED
+
 fi
 
 echo "STARTUP WEB SERVER..."
